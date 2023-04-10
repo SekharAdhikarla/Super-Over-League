b802e694540ea608eb446b6dd4addcf0630e65cb
SekharAdhikarla <Adhikarlavenkatasekhar@gmail.com>
2023-04-10 07:07:48 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="super">
+    <h1 className="heading">Super over league</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="logo"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
